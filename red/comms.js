7ce1c5ca1b6ad7684a76136e69ab01da2a9fcd93
waleed samy <waleed_samy@ymail.com>
2015-04-02 15:11:56 +0200

    kog we socket path

--- red/comms.js
+++ red/comms.js
@@ -46,6 +46,8 @@ function start() {
             var path = settings.httpAdminRoot || "/";
             path = (path.slice(0,1) != "/" ? "/":"") + path + (path.slice(-1) == "/" ? "":"/") + "comms";
             wsServer = new ws.Server({server:server,path:path});
+
+            console.log("Web Socket Path : "+path);
             
             wsServer.on('connection',function(ws) {
                 var pendingAuth = (settings.adminAuth != null);
@@ -114,13 +116,13 @@ function start() {
                     log.warn("comms error : "+err.toString());
                 });
             });
-            
+
             wsServer.on('error', function(err) {
                 log.warn("comms server error : "+err.toString());
             });
-             
+
             lastSentTime = Date.now();
-            
+
             heartbeatTimer = setInterval(function() {
                 var now = Date.now();
                 if (now-lastSentTime > webSocketKeepAliveTime) {
