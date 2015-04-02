a62bd35aab27c48b08fe37d011797397539286ae
waleed samy <waleed_samy@ymail.com>
2015-04-02 15:20:44 +0200

    change web socket url on production

--- red/comms.js
+++ red/comms.js
@@ -44,11 +44,15 @@ function start() {
         Users.default().then(function(anonymousUser) {
             var webSocketKeepAliveTime = settings.webSocketKeepAliveTime || 15000;
             var path = settings.httpAdminRoot || "/";
-            path = (path.slice(0,1) != "/" ? "/":"") + path + (path.slice(-1) == "/" ? "":"/") + "comms";
+            path = (path.slice(0,1) != "/" ? "/":"") + path + (path.slice(-1) == "/" ? "":"/");
+            if(process.env.NODE_ENV=="production"){
+                path+= "design/comms";
+            }else{
+                path+= "comms";
+            }
             wsServer = new ws.Server({server:server,path:path});
 
             console.log("Web Socket Path : "+path);
-            
             wsServer.on('connection',function(ws) {
                 var pendingAuth = (settings.adminAuth != null);
                 if (!pendingAuth) {
