b9e82ae1e65f6a47e1ad556f0c47af426600fbfe
waleed samy <waleed_samy@ymail.com>
2015-04-06 18:41:20 +0200

    handle uplaod my self using form data and ajax

--- red/upload/common.js
+++ red/upload/common.js
@@ -1,15 +1,8 @@
 var util = require('util');
 
 exports.uploadImage = function (req, res) {
-    var file = req.files['file_data'];
+    var file = req.files.file;
     console.log('file info: ', {name: file.name, path: file.path});
     res.set('Content-Type', 'application/json');
-    //allowed return : initialPreview ,initialPreviewConfig ,error
-    res.send({
-        initialPreview: ['<img src=uploads/' + file.name + ' class="file-preview-image" alt=test title=test>'],
-        initialPreviewConfig: [{
-            'caption': file.originalname, 'width': '120px', 'url': 'uploads/' + file.name, 'key': file.originalname
-        }],
-        append: true
-    });
+    res.send({url: "uploads/"+file.name});
 }
