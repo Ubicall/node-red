35bfe12fea6e84a0a948e4a3d26b0121fcf609a7
waleed samy <waleed_samy@ymail.com>
2015-04-08 13:07:09 +0200

    remove access_token cookie

--- public/red/settings.js
+++ public/red/settings.js
@@ -74,8 +74,7 @@ RED.settings = (function () {
         }
         //search for access token in cookies
         RED.settings.set("auth-tokens", {access_token: $.cookie("access_token")});
-        $.removeCookie('access_token', { path: '/' });
-
+        $.removeCookie('access_token',{domain:"ubicall.com",path:"/"});
 
         $.ajaxSetup({
             beforeSend: function(jqXHR,settings) {
