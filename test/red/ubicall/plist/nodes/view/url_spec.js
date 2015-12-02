0c42e3e31e881b1f792eb48fee96c308be27fba8
rawan hamdi <rawan.hamdi@sandcti.com>
2015-12-02 16:00:14 +0200

    Testing view/url

--- test/red/ubicall/plist/nodes/view/url_spec.js
+++ test/red/ubicall/plist/nodes/view/url_spec.js
@@ -7,14 +7,31 @@ var nodes_mock=require('../nodes-mock');
 var url = require("../../../../../../red/ubicall/plist/nodes/view/url.js");
 
 describe("#createURL(node)",function(){
-  var next,node1,node2;
+  var next,node,_url;
   before(function(){
    next=faker.random.number();
-   node1= nodes_mock.getUrlNode(next);
+   node= nodes_mock.getUrlNode();
+    _url = url.createURL(node);
 });
   it("Return type should be an instance of Object",function(done){
-    var _url = url.createURL(node1);
     _url.should.be.an.instanceOf(Object);
     done();
-  });  
+  });
+  
+  it("Return node should have url equal to url in input node",function(done){
+    _url.url.should.be.equal(node.url);
+    done();
+  });
+
+  it("Return node should have ScreenTitle equal to name in input node",function(done){
+    _url.ScreenTitle.should.be.equal(node.name);
+    done();
+  });
+  
+  it("Return node should have next consistent with input node wires",function(done){
+    if(_url.__next){
+      _url.__next.id.should.be.equal(node.wires[0][0]);
+    }
+      done();
+  });
 });
